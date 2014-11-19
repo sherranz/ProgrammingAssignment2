320a73cef5b156a2931d61ee13f44b1acdab1ad1
sherranz <sherranz.ag@gmail.com>
2014-11-19 01:55:43 +0100

    Coursera Programming Assignment 2 correction

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file allows to solve and cache the inverse of a matrix. First 
+## the matrix stores in an object, with a list of functions to set and get the 
+## matrix and its inverse. Then you can call cacheSolve to get the inverse. 
+## First time it will be calculated and stored, next times it will get from 
+## cache.
 
-## Given a matrix x, makeCacheMatrix returns a list of functions to cache its inverse
-## inverse will store the inverse matrix of x
+## Given a matrix x, makeCacheMatrix returns a list of functions to cache its inverse.
+## 'inverse' will store the inverse matrix of x
 
 makeCacheMatrix <- function(x = matrix()) {
   inverse<-NULL
@@ -20,7 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Solves the inverse matrix and store for firther use with setinv
+## Solves the inverse matrix and store for further use with the 'setinv' defined before
 
 cacheSolve <- function(x, ...) {
   inv<-x$getinv()
